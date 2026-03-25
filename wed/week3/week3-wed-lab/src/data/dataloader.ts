1edf301afd4bf308006f4a0f8cacf8f9a9a5a729
PrakashAdhikari08 <prakashadhikarijhapali@gmail.com>
2026-03-25 20:32:48 +1100

    user sign in feature check in or completed

--- wed/week3/week3-wed-lab/src/data/dataloader.ts
+++ wed/week3/week3-wed-lab/src/data/dataloader.ts
@@ -4,11 +4,11 @@
 import User from "@/data/user.data";
 
 let users  = [
-    {userName : "prakash", password : "<PASSWORD>", email : "<EMAIL>", role : "admin", isLoggedIN : true},
-    {userName : "Patrick", password : "<PASSWORD>", email : "<EMAIL>", role : "admin", isLoggedIN : true},
-    {userName : "Hill", password : "<PASSWORD>", email : "<EMAIL>", role : "admin", isLoggedIN : true},
-    {userName : "RMITuser", password : "<PASSWORD>", email : "<EMAIL>", role : "admin", isLoggedIN : true},
-    {userName : "testuser", password : "<PASSWORD>", email : "<EMAIL>", role : "admin", isLoggedIN : true}
+    {username : "prakash@gmail.com", password : "test@123", email : "<EMAIL>", role : "admin", isLoggedIN : true},
+    {username : "Patrick@gmail.com", password : "test@123", email : "<EMAIL>", role : "admin", isLoggedIN : true},
+    {username : "Hill@gmail.com", password : "test@123", email : "<EMAIL>", role : "admin", isLoggedIN : true},
+    {username : "rmit@gmail.com", password : "test@123", email : "<EMAIL>", role : "admin", isLoggedIN : true},
+    {username : "testuser@gmail.com", password : "test@123", email : "<EMAIL>", role : "admin", isLoggedIN : true}
 ]
  const  loadData = () => {
     localStorage.setItem("users", JSON.stringify(users));
